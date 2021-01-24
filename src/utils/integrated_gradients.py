17465322a83291d39648fd723282cf02ac777d56
Gunjan Chhablani <chhablani.gunjan@gmail.com>
2021-01-25 03:12:32 +0530

    Fix bugs in IG.

--- src/utils/integrated_gradients.py
+++ src/utils/integrated_gradients.py
@@ -593,6 +593,7 @@ class BertIntegratedGradients:
         batch_size = len(importances[0][0])
 
         # num_batches, num_layers, num_samples, 2 -> num_layers, num_samples*num_batches, 2
+        num_samples = 0
         layer_wise = [[] for _ in range(num_layers)]
         for batch_idx in range(num_batches):
             for layer_idx in range(num_layers):
@@ -602,9 +603,10 @@ class BertIntegratedGradients:
                     layer_wise[layer_idx].append(
                         importances[batch_idx][layer_idx][sample_idx]
                     )
+                    num_samples += 1
 
         # num_layers, num_samples, 2 -> num_samples, num_layers, 2
-        sample_wise = [[] for _ in range(num_batches * batch_size)]
+        sample_wise = [[] for _ in range(num_samples)]
         for layer_idx in range(num_layers):
             for sample_idx in range(len(layer_wise[layer_idx])):
                 sample_wise[sample_idx].append(layer_wise[layer_idx][sample_idx])
