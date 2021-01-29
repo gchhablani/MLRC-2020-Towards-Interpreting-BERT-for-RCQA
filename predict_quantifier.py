Fix Confidence Calculation

predict_quantifier.py
@@ -23,7 +23,7 @@ import torch
 from datasets import Dataset, load_metric
 import nltk
 import numpy as np
-import scipy.special
+from scipy.special import softmax
 
 
 nltk.download("averaged_perceptron_tagger")
@@ -193,28 +193,18 @@ quantifier_start_logits, quantifier_end_logits = quantifier_predictions.predicti
 
 
 quantifier_confidence = np.mean(
-    np.max(
-        scipy.special.softmax(quantifier_start_logits + quantifier_end_logits, axis=-1),
-        axis=-1,
-    )
+    np.max(softmax(quantifier_start_logits, axis=-1), axis=-1)
+    + np.max(softmax(quantifier_end_logits, axis=-1), axis=-1)
 )
 
 quantifier_numerical_confidence = np.mean(
-    np.max(
-        scipy.special.softmax(
-            quantifier_numerical_start_logits + quantifier_numerical_end_logits, axis=-1
-        ),
-        axis=-1,
-    )
+    np.max(softmax(quantifier_numerical_start_logits, axis=-1), axis=-1)
+    + np.max(softmax(quantifier_numerical_end_logits, axis=-1), axis=-1)
 )
 
 nonquantifier_confidence = np.mean(
-    np.max(
-        scipy.special.softmax(
-            nonquantifier_start_logits + nonquantifier_end_logits, axis=-1
-        ),
-        axis=-1,
-    )
+    np.max(softmax(nonquantifier_start_logits, axis=-1), axis=-1)
+    + np.max(softmax(nonquantifier_end_logits, axis=-1), axis=-1)
 )
 
 print(f"Quantifier Confidence: {quantifier_confidence}")